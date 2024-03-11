81f671145783458c34a3346467a399bfff52f399
Sasidharan2152 <142795410+Sasidharan2152@users.noreply.github.com>
2024-03-11 16:05:36 +0530

    next line

---
+++ sample1.java
@@ -0,0 +1,14 @@
+package mypack;
+import java.util.*;
+public class sample1{
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your name:");
+		String n = input.nextLine();
+		System.out.println("Hello "+n);
+		
+	}
+
+}
+
